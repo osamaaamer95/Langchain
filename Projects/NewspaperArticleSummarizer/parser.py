c23fc29630dd240d27074cea15fffcc7aeed0bd3
Osama Aamer <osama.aamer95@gmail.com>
2023-07-23 20:49:51 +0500

    Improve newspaper summarizer by adding few shot prompting and parser

---
+++ Projects/NewspaperArticleSummarizer/parser.py
@@ -0,0 +1,16 @@
+from langchain.output_parsers import PydanticOutputParser
+from pydantic import validator
+from pydantic import BaseModel, Field
+from typing import List
+
+
+class ArticleSummary(BaseModel):
+    title: str = Field(description="Title of the article")
+    summary: List[str] = Field(description="Bulleted list summary of the article")
+
+    # validating whether the generated summary has at least three lines
+    @validator('summary', allow_reuse=True)
+    def has_three_or_more_lines(cls, list_of_lines):
+        if len(list_of_lines) < 3:
+            raise ValueError("Generated summary has less than three bullet points!")
+        return list_of_lines
--- Projects/NewspaperArticleSummarizer/summarize.py
+++ Projects/NewspaperArticleSummarizer/summarize.py
@@ -1,10 +1,14 @@
+from langchain import PromptTemplate
+from langchain.output_parsers import PydanticOutputParser, RetryWithErrorOutputParser, OutputFixingParser
 from langchain.schema import (
-    SystemMessage, HumanMessage
+    SystemMessage, HumanMessage, OutputParserException
 )
 from langchain.chat_models import ChatOpenAI
 
+from Projects.NewspaperArticleSummarizer.parser import ArticleSummary
 
-def get_messages(article):
+
+def get_messages(article, parser):
     # prepare template for prompt
     template = """    
     Here's the article you will summarize:
@@ -15,25 +19,62 @@ def get_messages(article):
     {article_text}
     ==================
     
-    Write a summary for this article as a bulleted list.
+    Write a summary for this article as a bulleted list. 
+    
+    {format_instructions}
     """
 
-    prompt = template.format(article_title=article['title'], article_text=article['text'])
+    prompt = PromptTemplate(
+        template=template,
+        input_variables=["article_title", "article_text"],
+        partial_variables={"format_instructions": parser.get_format_instructions()}
+    )
+
+    formatted_prompt = prompt.format_prompt(article_title=article['title'], article_text=article['text'])
 
     return [
-        SystemMessage(content="You are an assistant that summarizes online articles."),
-        HumanMessage(content=prompt)
+        SystemMessage(
+            content="""
+            You are an assistant that summarizes online articles into bulleted points.
+            Here are a few examples of summarized articles:
+            Example 1:
+            Original Article: 'The Effects of Climate Change
+            Summary:
+            - Climate change is causing a rise in global temperatures.
+            - This leads to melting ice caps and rising sea levels.
+            - Resulting in more frequent and severe weather conditions.
+            
+            Example 2:
+            Original Article: 'The Evolution of Artificial Intelligence
+            Summary:
+            - Artificial Intelligence (AI) has developed significantly over the past decade.
+            - AI is now used in multiple fields such as healthcare, finance, and transportation.
+            - The future of AI is promising but requires careful regulation.                        
+            """
+        ),
+        HumanMessage(content=formatted_prompt.to_string())
     ]
 
 
 def summarize(article):
     # load the model
-    chat = ChatOpenAI(model_name="gpt-4", temperature=0)
+    chat = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
+
+    # set up output parser
+    parser = PydanticOutputParser(pydantic_object=ArticleSummary)
 
     # get messages
-    messages = get_messages(article)
+    messages = get_messages(article, parser)
 
     # generate
     summary = chat(messages)
 
-    return summary.content
+    try:
+
+        parsed_output = parser.parse(summary.content).json()
+        return parsed_output
+    except OutputParserException as e:
+        print("Output has less than three bullet points. Fixing output...")
+        # output fixing parser
+        outputfixing_parser = OutputFixingParser.from_llm(parser=parser, llm=chat)
+        print(outputfixing_parser.parse(summary.content).json())
